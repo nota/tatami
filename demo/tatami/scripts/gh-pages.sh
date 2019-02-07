d780c49ec1c0b8a59dfc41dc69d8ad1c86570ebc
daiiz <da.iz.mg@gmail.com>
2019-02-07 14:47:35 +0900

    npm install, build

--- demo/tatami/scripts/gh-pages.sh
+++ demo/tatami/scripts/gh-pages.sh
@@ -1,11 +1,11 @@
 rm -r ./gh-pages
 mkdir gh-pages
 
-# npm install
-# npm run build
+npm install
+npm run build
 
 cp -r public gh-pages/public
 cp index.html gh-pages/.
 cp default.html gh-pages/.
 
-#
+# gh-pages -d gh-pages
