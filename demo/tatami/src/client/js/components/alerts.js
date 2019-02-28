2a49f7ad9ab3223e7ef95c566158a71a723929d1
Isshu Rakusai <rakusai@kamilabo.jp>
2019-02-28 13:19:22 +0900

    alertを適用
    .alert-dismissibleの内容を消す
    
    文字列変更
    
    formのhas error stateの色が崩れるのでalertは別にする
    
    closeボタンの色とサイズを調整

--- demo/tatami/src/client/js/components/alerts.js
+++ demo/tatami/src/client/js/components/alerts.js
@@ -4,32 +4,79 @@ export default function Alerts () {
   return (
     <div>
       <h2>Alerts</h2>
-      <div className='alert alert-dismissible alert-success' role='alert'>
-        Well done!
-        {' '}
-        <a href='javascript:;' className='alert-link'>Link</a>
+      <div className='alert alert-default' role='alert'>
+        <div className='alert-body'>
+          Default. This is sample text.
+          {' '}
+        </div>
+        <button type='button' className='btn btn-default'>
+          Button
+        </button>
+      </div>
+      <div className='alert alert-info' role='alert'>
+        <div className='alert-body'>
+          Info. Heads up!
+          {' '}
+          <a href='javascript:;' className='alert-link'>Link</a>
+        </div>
+        <button type='button' className='btn btn-default'>
+          Button
+        </button>
+      </div>
+      <div className='alert alert-success' role='alert'>
+        <div className='alert-body'>
+          Success. Well done!
+          {' '}
+          <a href='javascript:;' className='alert-link'>Link</a>
+        </div>
+        <button type='button' className='btn btn-default'>
+          Button
+        </button>
         <button type='button' className='close' data-dismiss='alert' aria-label='Close'>
           <span aria-hidden='true'>&times;</span>
         </button>
       </div>
-      <div className='alert alert-dismissible alert-info' role='alert'>
-        Heads up!
-        {' '}
-        <a href='javascript:;' className='alert-link'>Link</a>
-        <button type='button' className='close' data-dismiss='alert' aria-label='Close'>
-          <span aria-hidden='true'>&times;</span>
-        </button>
-      </div>
+
       <div className='alert alert-warning' role='alert'>
-        Warning!
-        {' '}
-        <a href='javascript:;' className='alert-link'>Link</a>
+        <div className='alert-body'>
+          Warning. Be careful!
+          {' '}
+          <a href='javascript:;' className='alert-link'>Link</a>
+        </div>
       </div>
       <div className='alert alert-danger' role='alert'>
-        Oh snap!
-        {' '}
-        <a href='javascript:;' className='alert-link'>Link</a>
+        <div className='alert-body'>
+          Danger. Oh snap!
+          {' '}
+          <a href='javascript:;' className='alert-link'>Link</a>
+        </div>
+        <button type='button' className='close' data-dismiss='alert' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
       </div>
+
+      <h3>With heading</h3>
+      <div className='alert alert-default' role='alert'>
+        <div className='alert-body'>
+          <h3><strong>Heading title</strong></h3>
+          Effects present letters inquiry no an removed or friends. Desire behind latter me though in. Supposing shameless am he engrossed up additions. My possible peculiar together to. Desire so better am cannot he up before points. Remember mistaken opinions it pleasure of debating.
+        </div>
+      </div>
+
+      <h3>For small display</h3>
+      <div className='alert alert-default alert-vertical' role='alert'>
+        <div className='alert-body'>
+          Default. This is sample text.
+          {' '}
+        </div>
+        <button type='button' className='btn btn-default btn-lg btn-block'>
+          Action
+        </button>
+        <button type='button' className='btn btn-default btn-lg btn-block'>
+          Dismiss
+        </button>
+      </div>
+
     </div>
   )
 }
--- demo/tatami/src/client/js/components/forms.js
+++ demo/tatami/src/client/js/components/forms.js
@@ -161,6 +161,49 @@ export default function Forms () {
         <br />
         <br />
 
+        <h3>Validation states</h3>
+        <div className="form-group has-success">
+          <label className="control-label" for="inputSuccess1">Input with success</label>
+          <input type="text" className="form-control" id="inputSuccess1" aria-describedby="helpBlock2" />
+          <span id="helpBlock2" className="help-block">A block of help text that breaks onto a new line and may extend beyond one line.</span>
+        </div>
+        <div className="form-group has-warning">
+          <label className="control-label" for="inputWarning1">Input with warning</label>
+          <input type="text" className="form-control" id="inputWarning1" />
+        </div>
+        <div className="form-group has-error">
+          <label className="control-label" for="inputError1">Input with error</label>
+          <input type="text" className="form-control" id="inputError1" />
+        </div>
+        <div className="has-success">
+          <div className="checkbox">
+            <label>
+              <input type="checkbox" id="checkboxSuccess" value="option1" />
+              Checkbox with success
+            </label>
+          </div>
+        </div>
+        <div className="has-warning">
+          <div className="checkbox">
+            <label>
+              <input type="checkbox" id="checkboxWarning" value="option1" />
+              Checkbox with warning
+            </label>
+          </div>
+        </div>
+        <div className="has-error">
+          <div className="checkbox">
+            <label>
+              <input type="checkbox" id="checkboxError" value="option1" />
+              Checkbox with error
+            </label>
+          </div>
+        </div>
+
+        <br /><br />
+        <h3>Progress & Uplaod</h3>
+
+
         <div className='form-group'>
           <label htmlFor='slider'>Process nice score from -20 to +20</label>
           <input type='range' min='-20' max='20' defaultValue='0' data-require-focus-visible-class />
